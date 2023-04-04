9869b6f8236af598c7076b4f7984b59b40aa936f
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-04 23:54:34 +0600

    Footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Header></Header>
       <Outlet></Outlet>
-      
+      <Footer></Footer>
       
     </div>
   )
---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className=' mt-7 px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
+            <div className='py-6 text-sm text-center text-gray-400'>
+                © 2023 <a href="https://github.com/saifulislam1431"><span className='text-sky-600 font-medium'>Saiful Islam</span></a>. All rights reserved.
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
