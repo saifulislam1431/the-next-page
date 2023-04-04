9e049886f0a8caae2670c6a77248fa470813378f
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-05 04:06:05 +0600

    Updated

--- src/component/ErrorPage/ErrorPage.jsx
+++ src/component/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom';
 import Lottie from "lottie-react";
 import notFound from "../../assets/36395-lonely-404.json";
 
 const ErrorPage = () => {
+    const {error, status} = useRouteError();
     const navigate = useNavigate()
     const handlePrevious = ()=>{
         navigate(-1);
@@ -11,7 +12,10 @@ const ErrorPage = () => {
     return (
         <div className='  flex flex-col lg:flex-row justify-center items-center min-h-[100vh]'>
             <div >
-                <h1 className='font-extrabold text-5xl text-red-600'>Page Not Found!</h1>
+                <h1 className='font-extrabold text-5xl text-red-600'>{error}Page Not Found!</h1>
+
+                <p className='text-lg font-semibold text-red-500 mt-4'>{status}</p>
+
                 <button className='py-2 px-5 mt-5 bg-emerald-600 rounded-lg font-medium text-xl text-yellow-50' onClick={handlePrevious}>Go Back</button>
             </div>
             <div>
